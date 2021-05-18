d96778afdd13cd090ab85e72bba3063453493a01
Jai Kumar Das <jai.kumar@dhira.io>
2021-05-18 12:08:06 +0530

    added flask support and added job schedular

--- app.py
+++ app.py
@@ -2,6 +2,7 @@
 from colorama import init
 init()
 from cowin_api import CoWinAPI
+from fake_useragent import UserAgent
 import datetime
 import os
 import sys
@@ -12,15 +13,38 @@ from tabulate import tabulate
 from terminaltables import AsciiTable
 from colorama import Fore, Back, Style
 import requests
+import time
+import atexit
+from flask import Flask
+from flask_apscheduler import APScheduler
+from apscheduler.schedulers.background import BackgroundScheduler
+
+
+class Config:
+    SCHEDULER_API_ENABLED = True
+    JOBS = [
+    {
+        "id": "job1",
+        "func": "app:getSlotDetailsByPin",
+        "args": ("782103","14-05-2021",18),
+        "trigger": "interval",
+        "seconds": 50,
+    },
+        {
+        "id": "job2",
+        "func": "app:getSlotDetailsByPin",
+        "args": ("786001","14-05-2021",18),
+        "trigger": "interval",
+        "seconds": 50,
+    }
+    ]
 
 
 cowin = CoWinAPI()
-
-
-
-
-
-
+recipients = {
+"phone_number_list" : {"782103": ["8638151279","8638787934","8472832644","8622422198"],"786001":["8472832644"]},
+"whatsapp_number_list":{"782103":["8402063413","8638787934","8812914584","8472832644"],"786001":["8472832644"]}
+}
 with open('config.json') as f:
   config  = json.load(f)
 
@@ -36,7 +60,7 @@ def genererate_template(age,name,district,block_name,dates,slots,pin):
 
 
 def genererate_template_pin(age,name,district,block_name,start_time,end_time,pin,date):
-    template_message  = f"Over Age {age} years , Slot for district {district} is available at block {block_name} pin No. {pin} in {name} ; from {start_time} to {end_time}.Date {date}"
+    template_message  = f"Hi Billa, Over Age {age} years , Slot for district {district} is available at block {block_name} pin No. {pin} in {name} ; from {start_time} to {end_time}.Date {date}"
     return template_message
 
 
@@ -79,10 +103,11 @@ def getDatesAndSlotsSession(sessions_response):
     dates,slots_ = sessions_response["date"],",".join(sessions_response["slots"])
     return (dates,slots_)
 
-
 def getSlotDetailsByPin(pin_no,date,min_age_limit = 45):
+    user_agent = UserAgent()
+    headers = {'User-Agent': user_agent.random}
     URL = "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByPin?pincode={}&date={}".format(pin_no, date)
-    available_centers =  json.loads(requests.get(URL).text)
+    available_centers =  json.loads(requests.get(URL,headers = headers).text)
     print(available_centers)
     if len(available_centers["sessions"]) < 1:
         print(f"Slots not available for date {date} at pin no {pin_no}")
@@ -98,6 +123,11 @@ def getSlotDetailsByPin(pin_no,date,min_age_limit = 45):
             print(Fore.GREEN + Style.DIM + genererate_template_pin(age,name,district,block_name,start_time,end_time,pin,date))
             template_message = genererate_template_pin(age,name,district,block_name,start_time,end_time,pin,date)
             print(Style.RESET_ALL)
+            # Sending SMS 
+            for number in recipients["phone_number_list"][pin]:
+                sent_text_message(config,number,age,name,district,block_name,dates,slots,pin,template_message)
+            for num in recipients["whatsapp_number_list"][pin]:
+                sent_whatapp_message(config,num,age,name,district,block_name,dates,slots,pin,template_message)
             # sent_text_message(config,"8472832644",age,name,district,block_name,dates,slots,pin,template_message)
             # sent_whatapp_message(config,"8472832644",age,name,district,block_name,dates,slots,pin,template_message)
         return 1
@@ -142,8 +172,22 @@ print(date_str)
 state_id = 4  # for Assam
 
 # getSlotDetailsByPin("782103",date_str[0],45)
-getSlotDetailsByDistrictId("Nagaon",date_str[0],min_age_limit = 18)
+# getSlotDetailsByDistrictId("Nagaon",date_str[0],min_age_limit = 18)
 # districts = cowin.get_districts(state_id)
+
+
+
+# Shut down the scheduler when exiting the app
+# atexit.register(lambda: scheduler.shutdown())
+
+if __name__ == "__main__":
+    app = Flask(__name__)
+    app.config.from_object(Config())
+    scheduler = APScheduler()
+    # scheduler.add_job(func=getSlotDetailsByPin, trigger="interval", seconds=60)
+    scheduler.init_app(app)
+    scheduler.start()
+    app.run()
 
 
 
